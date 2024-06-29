82b4b443a27e7f67e10b9f2229d48c881e232232
feliperdamaceno <feliperdamaceno@sent.com>
2024-06-29 01:55:37 +0100

    feat: refine status balance

--- src/store/game.ts
+++ src/store/game.ts
@@ -19,8 +19,8 @@ export const BASE_HEALTH = 100
 export const BASE_HAPPYNESS = 100
 export const BASE_HUNGER = 100
 
-const TICK_TIME = 1000 // milliseconds
-const BASE_MULTIPLIER = 0.5 // multiples of 5
+const TICK_TIME = 1500 // milliseconds
+const BASE_MULTIPLIER = 1 // multiples of 5
 
 interface GameState {
   pet: Pet
@@ -68,25 +68,6 @@ export const useGameStore = create<GameState>((set, get) => ({
     }, TICK_TIME)
   },
 
-  feed: () => {
-    toggleStatus(PET_STATUS.EATING)
-
-    const interval = setInterval(() => {
-      if (get().pet.hunger === BASE_HUNGER) {
-        set(({ pet }) => ({ pet: { ...pet, status: PET_STATUS.IDLE } }))
-        return clearInterval(interval)
-      }
-
-      if (get().pet.status === PET_STATUS.IDLE) {
-        return clearInterval(interval)
-      }
-
-      set(({ pet }) => ({
-        pet: { ...pet, hunger: pet.hunger + BASE_MULTIPLIER * 2 }
-      }))
-    }, TICK_TIME)
-  },
-
   play: () => {
     toggleStatus(PET_STATUS.PLAYING)
 
@@ -108,7 +89,26 @@ export const useGameStore = create<GameState>((set, get) => ({
       }
 
       set(({ pet }) => ({
-        pet: { ...pet, energy: pet.energy - BASE_MULTIPLIER * 1 }
+        pet: { ...pet, energy: pet.energy - BASE_MULTIPLIER * 2 }
+      }))
+    }, TICK_TIME)
+  },
+
+  feed: () => {
+    toggleStatus(PET_STATUS.EATING)
+
+    const interval = setInterval(() => {
+      if (get().pet.hunger === BASE_HUNGER) {
+        set(({ pet }) => ({ pet: { ...pet, status: PET_STATUS.IDLE } }))
+        return clearInterval(interval)
+      }
+
+      if (get().pet.status === PET_STATUS.IDLE) {
+        return clearInterval(interval)
+      }
+
+      set(({ pet }) => ({
+        pet: { ...pet, hunger: pet.hunger + BASE_MULTIPLIER * 2 }
       }))
     }, TICK_TIME)
   },
